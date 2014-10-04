27a5ff6a01292a077f50717b0848842d17c9663a
Shao-Chuan Lee <lsc36x@gmail.com>
2014-10-04 23:23:18 +0800

    dataset class

---
+++ libact/base/dataset.py
@@ -0,0 +1,26 @@
+"""
+The dataset class used in this package.
+Datasets consists of data used for training, represented by a list of
+(feature, label) tuples.
+May be exported in different formats for application on other libraries.
+"""
+
+class Dataset(object):
+
+    def __init__(self):
+        self.data = []
+
+    def add(self, feature, label):
+        """Add a (feature, label) entry into the dataset.
+        A None label indicates an unlabeled entry.
+        """
+        self.data.append((feature, label))
+
+    # TODO: remove, update
+
+    def format_sklearn(self):
+        """Returns dataset in (X, y) format for use in scikit-learn.
+        Unlabeled entries are ignored.
+        """
+        l = list(zip(*[entry for entry in self.data if entry[1] is not None]))
+        return (list(l[0]), list(l[1]))
