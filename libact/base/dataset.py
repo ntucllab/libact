850bcb82c77129666c3094fbc20155c52d55a136
yangarbiter <yangarbiter@gmail.com>
2014-11-09 23:25:19 +0800

    added method __len__ and uniform_sample in Dataset

--- libact/base/dataset.py
+++ libact/base/dataset.py
@@ -4,6 +4,7 @@ Datasets consists of data used for training, represented by a list of
 (feature, label) tuples.
 May be exported in different formats for application on other libraries.
 """
+import random
 
 class Dataset(object):
 
@@ -14,6 +15,10 @@ class Dataset(object):
     def __getitem__(self, key):
         """Allow list-like access: dataset[key]"""
         return self.data[key]
+
+    def __len__(self):
+        """Return the number of data entries in this object."""
+        return len(self.data)
 
     def add(self, feature, label):
         """Add a (feature, label) entry into the dataset.
@@ -38,3 +43,19 @@ class Dataset(object):
         """Returns list of entry_ids of unlabeled features"""
         return [entry_id for entry_id, entry in enumerate(self.data)
             if entry[1] == None]
+
+    def uniform_sample(self, samplesize, replace=True):
+        """Returns a Dataset object with is resampled uniformly with given
+        samplsize. Parameter replace decides whether sampling with replacement
+        or not"""
+        ret = Dataset()
+        if replace:
+            for i in xrange(samplesize):
+                ran = random.choice(self.data)
+                ret.add(ran[0], ran[1])
+            return ret
+        else:
+            ret.data = random.sample(self.data, samplesize)
+            return ret
+            
+
