4c731e93fe38f5fea27132605e673a8097c85742
Shao-Chuan Lee <lsc36x@gmail.com>
2015-04-09 11:38:17 +0800

    remove unused

--- libact/query_strategies/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup, Extension
-import numpy.distutils.misc_util
-setup(
-    ext_modules=[Extension("varRedu",
-                            ["variance_reduction.c"],
-                            extra_link_args=['-llapacke'],
-                            extra_compile_args=['-std=c11'])],
-    include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs(),
-)
