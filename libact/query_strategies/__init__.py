f9f15f173747a0580a1c698102c74522537d81ef
Shao-Chuan Lee <lsc36x@gmail.com>
2015-07-18 17:14:20 +0000

    not importing HintSVM if library not found (should work)

--- libact/query_strategies/__init__.py
+++ libact/query_strategies/__init__.py
@@ -2,7 +2,12 @@
 Concrete query strategy classes.
 """
 
-from .hintsvm import HintSVM
 from .uncertainty_sampling import UncertaintySampling
 from .query_by_committee import QueryByCommittee
 from .quire import QUIRE
+
+try:
+    from .hintsvm import HintSVM
+except ImportError:
+    # HintSVM library not found, not importing
+    pass
