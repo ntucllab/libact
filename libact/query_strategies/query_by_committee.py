7eb46ef9abe7e9100d6cc86b3a9196670241789f
yangarbiter <yangarbiter@gmail.com>
2015-07-20 23:34:54 +0800

    update docstring to numpydoc style

--- libact/query_strategies/query_by_committee.py
+++ libact/query_strategies/query_by_committee.py
@@ -15,14 +15,16 @@ class QueryByCommittee(QueryStrategy):
 
     Parameters
     ----------
-    models : list of libact.models
-        models to be included in the committee to vote for each instance.
+    models : list of libact.models instances or str
+        This parameter accepts a list of initialized libact Model instances,
+        or class names of libact Model classes to determine the models to be
+        included in the committee to vote for each unlabeled instance.
 
 
     Attributes
     ----------
     students : list, shape = [len(models)]
-        A list of model instances created from models.
+        A list of model instances used in this algorithm.
 
 
     References
@@ -30,13 +32,8 @@ class QueryByCommittee(QueryStrategy):
     Seung, H. Sebastian, Manfred Opper, and Haim Sompolinsky. "Query by
     committee." Proceedings of the fifth annual workshop on Computational
     learning theory. ACM, 1992.
-        """
-
+    """
     def __init__(self, *args, **kwargs):
-        """
-        model: a list of initialized libact Model instances, or class names of
-               libact Model classes for prediction.
-        """
         super(QueryByCommittee, self).__init__(*args, **kwargs)
         self.students = list()
         models = kwargs.pop('models', None)
