93c4876e2a737868fbb5bfe8f3aa3fe16de11edf
yangarbiter <yangarbiter@gmail.com>
2015-10-08 14:43:28 +0800

    remove redundent import

--- libact/query_strategies/__init__.py
+++ libact/query_strategies/__init__.py
@@ -5,9 +5,3 @@ Concrete query strategy classes.
 from .uncertainty_sampling import UncertaintySampling
 from .query_by_committee import QueryByCommittee
 from .quire import QUIRE
-
-try:
-    from .hintsvm import HintSVM
-except ImportError:
-    # HintSVM library not found, not importing
-    pass
