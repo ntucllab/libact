b6c4d217cd067fc038a84136633d604fbba68b74
yangarbiter <yangarbiter@gmail.com>
2015-11-07 16:58:07 +0800

    add HintSVM to __init__

--- libact/query_strategies/__init__.py
+++ libact/query_strategies/__init__.py
@@ -6,3 +6,4 @@ from .active_learning_by_learning import ActiveLearningByLearning
 from .uncertainty_sampling import UncertaintySampling
 from .query_by_committee import QueryByCommittee
 from .quire import QUIRE
+from .hintsvm import HintSVM
