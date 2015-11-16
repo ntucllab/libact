be76c9558183826c7f0bd38c3f254a44e84c79f6
Shao-Chuan Lee <lsc36x@gmail.com>
2015-11-16 20:07:24 +0800

    Random sampling query strategy

--- libact/query_strategies/__init__.py
+++ libact/query_strategies/__init__.py
@@ -5,4 +5,5 @@ Concrete query strategy classes.
 from .uncertainty_sampling import UncertaintySampling
 from .query_by_committee import QueryByCommittee
 from .quire import QUIRE
+from .random_sampling import RandomSampling
 from .variance_reduction import VarianceReduction
