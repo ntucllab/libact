7640f8986bfb6c999120956862dcf2d96e630d95
yangarbiter <yangarbiter@gmail.com>
2015-11-18 21:40:30 +0800

    __init__.py for libact module

---
+++ libact/__init__.py
@@ -0,0 +1,18 @@
+"""
+libact
+======
+
+Available subpackages
+---------------------
+base
+    Defines interface and dataset object.
+labelers
+    Interface for labeling data.
+models
+    Learning models.
+query_strategies
+    Implementation of active learning algorithms.
+
+"""
+
+__all__ = ["base", "labelers", "models", "query_strategies"]
