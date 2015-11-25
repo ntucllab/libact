c082bef564483eeb077ce16359b4c6ab93750440
wutungen <wu1988100483@gmail.com>
2015-11-25 19:06:19 +0800

    quire efficient version

--- libact/query_strategies/quire.py
+++ libact/query_strategies/quire.py
@@ -4,7 +4,7 @@
 """
 
 import bisect
-
+import time
 import numpy as np
 from sklearn.metrics.pairwise import rbf_kernel
 
@@ -26,6 +26,7 @@ class QUIRE(QueryStrategy):
         X, self.y = zip(*self.dataset.get_entries())
         self.y = list(self.y)
         K = rbf_kernel(X=X, Y=X, gamma=self.gamma)
+        self.K = K
         self.L = np.linalg.inv(K + self.lmbda * np.eye(len(X)))
 
     def update(self, entry_id, label):
@@ -34,15 +35,27 @@ class QUIRE(QueryStrategy):
         self.y[entry_id] = label
 
     def make_query(self):
+        #print("in quire_make_query")
         L = self.L
+        K = self.K
         Lindex = self.Lindex
+        len_Lindex = len(Lindex)
         Uindex = self.Uindex
+        len_Uindex = len(Uindex)
         query_index = -1
         min_eva = np.inf
         y_labeled = np.array([label for label in self.y if label is not None])
-        Laa = (((L[Uindex]).T)[Uindex]).T
+        Laa = L[Uindex, :][:, Uindex]
         det_Laa = np.linalg.det(Laa)
-        for each_index in Uindex:
+        """efficient computation of inv(Laa)
+        """
+        M3 =  np.dot( self.K[Uindex, :][:, Lindex],  np.linalg.inv( self.lmbda * np.eye(len_Lindex) ) )
+        M2 =  np.dot( M3, self.K[Lindex, :][:, Uindex] )
+        M1 = self.lmbda * np.eye(len_Uindex) + self.K[Uindex, :][:, Uindex]
+        inv_Laa = M1 - M2
+        i = -1
+        iList = [j for j in range(len_Uindex)]
+        for each_index in Uindex:            
             """go through all unlabeled instances and compute their evaluation
             values one by one
             """
@@ -51,17 +64,30 @@ class QUIRE(QueryStrategy):
             Uindex_r = Uindex[:]
             Uindex_r.remove(each_index)
             Lsu = L[each_index][Uindex_r]
-            Lul = (((L[Uindex_r]).T)[Lindex]).T
-            Luu = (((L[Uindex_r]).T)[Uindex_r]).T
-
+            Lul = L[Uindex_r, :][:, Lindex]
+            Luu = L[Uindex_r, :][:, Uindex_r]
+            """efficient computation of inv(Luu)
+            """
+            i = i + 1
+            iList_r = iList[:]
+            iList_r.remove(i)
+            a = inv_Laa[i, i]
+            b = -inv_Laa[iList_r, i]
+            D = inv_Laa[iList_r, :][:, iList_r]
+            inv_Luu = D - 1/a * np.dot( b,  b.T )
+            tmp = np.dot(
+                Lsl - np.dot(np.dot(Lsu, inv_Luu), Lul),
+                y_labeled,
+                )
+            """
             tmp = np.dot(
                 Lsl - np.dot(np.dot(Lsu, np.linalg.inv(Luu)), Lul),
                 y_labeled,
                 )
+            """
             eva = Lss - det_Laa / Lss + 2 * np.abs(tmp)
 
             if eva < min_eva:
                 query_index = each_index
                 min_eva = eva
-
         return query_index
