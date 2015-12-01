b9aadc05cf2eebe34dda99c47cc727743e911aa2
wutungen <wu1988100483@gmail.com>
2015-12-01 15:11:44 +0800

    [modification]modify ideal_labeler.py according to the comments

--- libact/labelers/ideal_labeler.py
+++ libact/labelers/ideal_labeler.py
@@ -1,14 +1,29 @@
 """
 Ideal/Noiseless labeler that returns true label
+
 """
 from libact.base.interfaces import Labeler
+import numpy as np
 
 class IdealLabeler(Labeler):
-    def __init__(self, dataset, true_y, **kwargs):
-        X, y = zip(*dataset.data)
-        feature = [list(x) for x in X]
-        self.feature = feature
-        self.true_y = true_y
+    """    
+    Provide the errorless/noiseless label to any feature vectors being queried.
+
+    Attributes
+    ----------
+    features: numpy array
+        an array of features used as the search keys for labels
+    
+    label: numpy array
+        an array of noiesless labels corresponding to the features
+    """ 
+
+    def __init__(self, dataset, **kwargs):
+        X, y = zip(*dataset.get_entries())
+        #make sure the input dataset is fully labeled
+        assert((np.array(y) != np.array(None)).all())
+        self.X = X
+        self.y = y
 
     def label(self, feature):
-        return self.true_y[self.feature.index(list(feature))]
+        return self.y[np.where([np.array_equal(x, feature) for x in self.X])[0]]
