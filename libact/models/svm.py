b496f8945cd655fec11cc21fe7b74e518a35dd21
Shao-Chuan Lee <lsc36x@gmail.com>
2015-12-02 21:52:25 +0800

    docs: make SVM doc preserve line breaks

--- libact/models/svm.py
+++ libact/models/svm.py
@@ -15,35 +15,35 @@ class SVM(Model):
     Parameters
     ----------
     param : string, optional, default='-t 0 -c 0.1 -b 0 -q'
-        libsvm paramter options:
-        -s svm_type : set type of SVM (default 0)
-            0 -- C-SVC
-            1 -- nu-SVC
-            2 -- one-class SVM
-            3 -- epsilon-SVR
-            4 -- nu-SVR
-        -t kernel_type : set type of kernel function (default 2)
-            0 -- linear: u'*v
-            1 -- polynomial: (gamma*u'*v + coef0)^degree
-            2 -- radial basis function: exp(-gamma*|u-v|^2)
-            3 -- sigmoid: tanh(gamma*u'*v + coef0)
-        -d degree : set degree in kernel function (default 3)
-        -g gamma : set gamma in kernel function (default 1/num_features)
-        -r coef0 : set coef0 in kernel function (default 0)
-        -c cost : set the parameter C of C-SVC, epsilon-SVR, and nu-SVR
-        (default 1)
-        -n nu : set the parameter nu of nu-SVC, one-class SVM, and nu-SVR
-        (default 0.5)
-        -p epsilon : set the epsilon in loss function of epsilon-SVR
-        (default 0.1)
-        -m cachesize : set cache memory size in MB (default 100)
-        -e epsilon : set tolerance of termination criterion (default 0.001)
-        -h shrinking: whether to use the shrinking heuristics, 0 or 1
-        (default 1)
-        -b probability_estimates: whether to train a SVC or SVR model for
-        probability estimates, 0 or 1 (default 0)
-        -wi weight: set the parameter C of class i to weight*C, for C-SVC
-        (default 1)
+        | libsvm paramter options:
+        | -s svm_type : set type of SVM (default 0)
+        |     0 -- C-SVC
+        |     1 -- nu-SVC
+        |     2 -- one-class SVM
+        |     3 -- epsilon-SVR
+        |     4 -- nu-SVR
+        | -t kernel_type : set type of kernel function (default 2)
+        |     0 -- linear: u'*v
+        |     1 -- polynomial: (gamma*u'*v + coef0)^degree
+        |     2 -- radial basis function: exp(-gamma*|u-v|^2)
+        |     3 -- sigmoid: tanh(gamma*u'*v + coef0)
+        | -d degree : set degree in kernel function (default 3)
+        | -g gamma : set gamma in kernel function (default 1/num_features)
+        | -r coef0 : set coef0 in kernel function (default 0)
+        | -c cost : set the parameter C of C-SVC, epsilon-SVR, and nu-SVR
+        | (default 1)
+        | -n nu : set the parameter nu of nu-SVC, one-class SVM, and nu-SVR
+        | (default 0.5)
+        | -p epsilon : set the epsilon in loss function of epsilon-SVR
+        | (default 0.1)
+        | -m cachesize : set cache memory size in MB (default 100)
+        | -e epsilon : set tolerance of termination criterion (default 0.001)
+        | -h shrinking: whether to use the shrinking heuristics, 0 or 1
+        | (default 1)
+        | -b probability_estimates: whether to train a SVC or SVR model for
+        | probability estimates, 0 or 1 (default 0)
+        | -wi weight: set the parameter C of class i to weight*C, for C-SVC
+        | (default 1)
 
     Attributes
     ----------
