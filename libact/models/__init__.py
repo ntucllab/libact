f41b10af6140e43c5dbf3f4f0b8381d62ff06566
yangarbiter <yangarbiter@gmail.com>
2015-12-17 11:30:49 +0800

    change models.svm from using libsvm to sklearn

--- libact/models/__init__.py
+++ libact/models/__init__.py
@@ -7,7 +7,4 @@ logger = logging.getLogger(__name__)
 
 from .logistic_regression import LogisticRegression
 from .perceptron import Perceptron
-try:
-    from .svm import SVM
-except ImportError:
-    logger.warn('libsvm python interface not found, SVM model not available.')
+from .svm import SVM
