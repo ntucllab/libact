414cb2d148d294596266c7657c10524c0ac6d88b
yangarbiter <b01902066@ntu.edu.tw>
2015-12-17 20:21:11 +0800

    Remove unused logger import

--- libact/models/__init__.py
+++ libact/models/__init__.py
@@ -1,10 +1,6 @@
 """
 Concrete model classes.
 """
-import logging
-
-logger = logging.getLogger(__name__)
-
 from .logistic_regression import LogisticRegression
 from .perceptron import Perceptron
 from .svm import SVM
