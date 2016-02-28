e91e84e6f57dfe07afe4f15f3f4439c86cef8ea4
yangarbiter <yangarbiter@gmail.com>
2016-02-29 00:38:31 +0800

    try to fix _variance_reduction error

--- docs/conf.py
+++ docs/conf.py
@@ -26,8 +26,7 @@ class Mock(MagicMock):
 
 MOCK_MODULES = ['scipy', 'matplotlib', 'numpy', 'sklearn',
                 'sklearn.linear_model', 'sklearn.svm', 'sklearn.metrics',
-                'sklearn.metrics.pairwise',
-                'libact.query_strategies._variance_reduction']
+                'sklearn.metrics.pairwise']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
