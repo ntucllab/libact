61422aebd4e2bb5ff34b534f352dc96d14e73b01
yangarbiter <yangarbiter@gmail.com>
2016-02-29 00:11:44 +0800

    add _variance_reduction dummy interface

--- libact/query_strategies/__init__.py
+++ libact/query_strategies/__init__.py
@@ -13,4 +13,5 @@ from .uncertainty_sampling import UncertaintySampling
 from .query_by_committee import QueryByCommittee
 from .quire import QUIRE
 from .random_sampling import RandomSampling
+from ._variance_reduction import estVar
 from .variance_reduction import VarianceReduction
---
+++ libact/query_strategies/_variance_reduction.py
@@ -0,0 +1,14 @@
+
+def estVar(sigma, PI_obj, X_obj, ePI_obj, eX_obj):
+    """
+    Estimate variance for variance_reduction.
+
+    Parameters
+    ----------
+    sigma: float
+        regularization term
+
+    Returns
+    -------
+    """
+    pass
