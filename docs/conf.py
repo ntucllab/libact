353d1c79a521df613c8ff34522e43b7fe50ffa77
yangarbiter <yangarbiter@gmail.com>
2016-02-28 22:29:59 +0800

    add mock in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,16 @@
 import sys
 import os
 import shlex
+import sys
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return Mock()
+
+MOCK_MODULES = ['scipy', 'matplotlib', 'numpy']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
