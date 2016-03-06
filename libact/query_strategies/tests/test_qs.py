7878509cbd83972d3f74ec32dc4f45bcf360d6f5
tungen <wu1988100483@gmail.com>
2016-03-07 01:38:34 +0800

    fix error

--- libact/query_strategies/tests/test_qs.py
+++ libact/query_strategies/tests/test_qs.py
@@ -2,15 +2,19 @@ from numpy.testing import assert_array_equal
 import numpy as np
 
 from libact.base.interfaces import QueryStrategy, ContinuousModel
+from libact.base.dataset import Dataset, import_libsvm_sparse
+from libact.models import *
+from libact.query_strategies import *
+from libact.labelers import IdealLabeler
 
-X = [[-2, -1], [-1, -1], [-1, -2], [1, 1], [1, 2], [2, 1]\
-    [-2, -2], [0, -2], [0, 1], [1.5, 1.5]]
+X = [[-2, -1], [-1, -1], [-1, -2], [1, 1], [1, 2], [2, 1], [-2, -2], [0, -2], \
+        [0, 1], [1.5, 1.5]]
 Y = [-1, -1, -1, 1, 1, 1, -1, -1, 1, 1]
 fully_labeled_trn_ds = Dataset(X, Y)
 lbr = IdealLabeler(fully_labeled_trn_ds)
 quota = 4    
 
-def init_toyexample()
+def init_toyexample():
     trn_ds = Dataset(X, np.concatenate([Y[:6], [None] * 4]))
     return trn_ds
 
@@ -26,14 +30,14 @@ def run_qs(trn_ds, lbr, model, qs, quota):
     return np.array(qseq)
 
     
-def test_uncertainty_lc
+def test_uncertainty_lc():
     trn_ds = init_toyexample()
     qs = UncertaintySampling(trn_ds, method='lc', model=LogisticRegression())
     model = LogisticRegression()
     qseq = run(trn_ds, lbr, model, qs, quota)
     assert_array_equal(qseq, np.array(Y[6:]))
     
-def test_uncertainty_sm
+def test_uncertainty_sm():
     trn_ds = init_toyexample()
     qs = UncertaintySampling(trn_ds, method='sm', model=LogisticRegression())
     model = LogisticRegression()
