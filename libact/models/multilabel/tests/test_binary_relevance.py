a370b765a9b53014040e1219062e4144d05df818
yangarbiter <yangarbiter@gmail.com>
2017-02-06 16:50:52 +0800

    add predict_proba test for binary_relevance

--- libact/models/multilabel/tests/test_binary_relevance.py
+++ libact/models/multilabel/tests/test_binary_relevance.py
@@ -27,6 +27,9 @@ class BinaryRelevanceTestCase(unittest.TestCase):
         br_pred_train = br.predict(self.X_train).astype(int)
         br_pred_test = br.predict(self.X_test).astype(int)
 
+        br_pred_proba_train = br.predict_proba(self.X_train).astype(float)
+        br_pred_proba_test = br.predict_proba(self.X_test).astype(float)
+
         for i in range(np.shape(self.Y_train)[1]):
             clf = sklearn.linear_model.LogisticRegression(random_state=1126)
             clf.fit(self.X_train, self.Y_train[:, i])
@@ -35,6 +38,11 @@ class BinaryRelevanceTestCase(unittest.TestCase):
                                br_pred_train[:, i])
             assert_array_equal(clf.predict(self.X_test).astype(int),
                                br_pred_test[:, i])
+
+            assert_array_equal(clf.predict_proba(self.X_train)[:, 1].astype(float),
+                               br_pred_proba_train[:, i])
+            assert_array_equal(clf.predict_proba(self.X_test)[:, 1].astype(float),
+                               br_pred_proba_test[:, i])
 
         self.assertEqual(
             np.mean(np.abs(self.Y_test - br_pred_test).mean(axis=1)),
