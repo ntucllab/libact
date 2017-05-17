5737930c158f2512ec4b4f1a480945aa0e598bd2
yangarbiter <yangarbiter@gmail.com>
2017-05-17 17:45:32 +0800

    bumping version to 0.1.3

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ else:
 
 setup(
     name='libact',
-    version='0.1.3b1',
+    version='0.1.3',
     description='Pool-based active learning in Python',
     long_description=open('README.md').read(),
     author='Y.-Y. Yang, S.-C. Lee, Y.-A. Chung, T.-E. Wu, H.-T. Lin',
